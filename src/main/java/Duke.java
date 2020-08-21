368285721df92579ecd4208a027c812979e78763
Olivier Cheah <oliviercheah@gmail.com>
2020-08-21 18:38:59 +0800

    level 0 commit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String command;
+        Scanner in = new Scanner(System.in);
+        List<String> tasks = new ArrayList<>();
+
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+////        System.out.println("Hello from\n" + logo);
+        String line = "----------------------------------------";
+        String helloMessage = "Hello! I'm Duke";
+        String greetMsg = "What can I do for you?";
+        String byeMsg = "Bye. Hope to see you again";
+
+        System.out.println(line);
+        System.out.println(helloMessage);
+        System.out.println(greetMsg);
+        System.out.println(line);
+
+        System.out.println(line);
+        System.out.println(byeMsg);
+        System.out.println(line);
+
+
     }
 }
